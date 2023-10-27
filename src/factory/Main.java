3e12174176d62dd94596604233f33cdce220b97c
ehu <ehu@U030080>
2023-10-27 16:49:18 +0200

    aaa

--- src/factory/Main.java
+++ src/factory/Main.java
@@ -6,8 +6,14 @@ import domain.Medicament;
 public class Main {
 
 	public static void main(String[] args) {
-		Covid19Pacient p1=new Covid19Pacient("aitor", 35);
+		SymptomFactory a = new SymptomFactory();
+		Covid19Pacient p1=new Covid19Pacient("aitor", 35,a);
 		new PacientSymptomGUI(p1);
+		
+		
+		Medicament m=new Medicament("Ibuprofeno", a);
+		MedicalGUI mgui=new MedicalGUI(m);
+		
 
 	}
 
